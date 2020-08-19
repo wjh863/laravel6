55d22068e5482755b05138007a0e086ab9b191f1
wjh863 <2824653863@qq.com>
2020-08-19 09:13:11 +0800

    11111

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('students', 'StudentController@students');
+Route::namespace('Admin')->group(function () {
+    // 在 「App\Http\Controllers\Admin」 命名空间下的控制器
+	Route::any('userlist', 'UserController@userlist');
+});
+
